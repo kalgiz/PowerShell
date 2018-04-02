9a2a4129df17a81562fda025263d2ef939a6ef40
kalgiz <klalgiz@microsoft.com>
2018-04-02 15:21:43 -0700

    Build stage

--- tools/vsts.sh
+++ tools/vsts.sh
@@ -2,6 +2,6 @@
 
 if [ $1 == "Bootstrap" ]; then
     pwsh -command ". ./vsts.ps1; Invoke-PSBootstrap"
-else
-    echo "Not bootstrap"
+if [ $1 == "Build" ]; then
+    pwsh -command ". ./vsts.ps1; Invoke-PSBuild"
 fi
