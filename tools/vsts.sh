1e331bfbcf79da437ac3d24c971e156af96e3f92
kalgiz <klalgiz@microsoft.com>
2018-04-02 15:24:14 -0700

    a

--- tools/vsts.sh
+++ tools/vsts.sh
@@ -2,6 +2,6 @@
 
 if [ $1 == "Bootstrap" ]; then
     pwsh -command ". ./vsts.ps1; Invoke-PSBootstrap"
-if [ $1 == "Build" ]; then
+elif [ $1 == "Build" ]; then
     pwsh -command ". ./vsts.ps1; Invoke-PSBuild"
 fi
