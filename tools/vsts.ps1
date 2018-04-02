784027f59dc16ac32bf1df8e231d6685a7505cda
kalgiz <klalgiz@microsoft.com>
2018-04-02 11:50:27 -0700

    a

--- tools/vsts.ps1
+++ tools/vsts.ps1
@@ -1,6 +1,8 @@
 Write-Host "Hooray bootstrap"
+Invoke-PSBootstrap
 
 function Invoke-PSBootstrap {
+    Write-Host "Invoke-PSBootstrap function called"
     # Write-Host -Foreground Green "Executing Linux vsts -BootStrap"
     # Write-Host -Foreground Green "Executing Linux vsts -BootStrap `$isPR='$isPr' - $commitMessage"
     # Make sure we have all the tags
