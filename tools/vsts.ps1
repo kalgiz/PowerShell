0d54eb3eebf348875ea868399ad85af9fcd44de6
kalgiz <klalgiz@microsoft.com>
2018-04-02 11:23:19 -0700

    Bootstrap phase

--- tools/vsts.ps1
+++ tools/vsts.ps1
@@ -1,1 +1,7 @@
-Write-Host "klaudia's stage!!"
+function Invoke-PSBootstrap {
+    Write-Host -Foreground Green "Executing Linux vsts -BootStrap"
+    # Write-Host -Foreground Green "Executing Linux vsts -BootStrap `$isPR='$isPr' - $commitMessage"
+    # Make sure we have all the tags
+    Sync-PSTags -AddRemoteIfMissing
+    Start-PSBootstrap
+}
--- tools/vsts.sh
+++ tools/vsts.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# originally it was installed only for linux
-nvm install 6.4.0 &&
-npm install -g markdown-spellcheck@0.11.0;
-
-pwsh -command "./vsts.ps1"
+pwsh -command "./vsts.ps1; Invoke-PSBootstrap"
