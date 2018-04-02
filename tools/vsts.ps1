7f8809b1a4e1a6e3ae796ad859c0febf7d1c9cf6
kalgiz <klalgiz@microsoft.com>
2018-04-02 12:02:09 -0700

    a

--- tools/vsts.ps1
+++ tools/vsts.ps1
@@ -1,10 +1,10 @@
 function Invoke-PSBootstrap {
     Write-Host "Invoke-PSBootstrap function called"
-    # Write-Host -Foreground Green "Executing Linux vsts -BootStrap"
+    Write-Host -Foreground Green "Executing Linux vsts -BootStrap"
     # Write-Host -Foreground Green "Executing Linux vsts -BootStrap `$isPR='$isPr' - $commitMessage"
     # Make sure we have all the tags
-    # Sync-PSTags -AddRemoteIfMissing
-    # Start-PSBootstrap
+    Sync-PSTags -AddRemoteIfMissing
+    Start-PSBootstrap
 }
 
 Write-Host "Hooray bootstrap"
