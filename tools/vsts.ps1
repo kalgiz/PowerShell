83d4eeef0afc650e64aeaf5fd7f87f1f134e2e0a
kalgiz <klalgiz@microsoft.com>
2018-04-02 15:00:39 -0700

    commit message

--- tools/vsts.ps1
+++ tools/vsts.ps1
@@ -1,10 +1,10 @@
 $repoRoot = Join-Path $PSScriptRoot '..'
 Import-Module (Join-Path $repoRoot 'build.psm1') -Scope Global
 $isPR = $env:BUILD_REASON -eq "PullRequest"
+$commitMessage = git log --format=%B -n 1 $env:BUILD_SOURCEVERSION
+
 function Invoke-PSBootstrap {
-    Write-Host $env:BUILD_SOURCEVERSION
-    Write-Host -Foreground Green "Executing Linux vsts -BootStrap"
-    # Write-Host -Foreground Green "Executing Linux vsts -BootStrap `$isPR='$isPr' - $commitMessage"
+    Write-Host -Foreground Green "Executing Linux vsts -BootStrap `$isPR='$isPr' - $commitMessage"
     # Make sure we have all the tags
     Sync-PSTags -AddRemoteIfMissing
     Start-PSBootstrap
