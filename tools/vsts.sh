b7261c0997c56c9a8de1a6f6fc3aa115cf62e44a
kalgiz <klalgiz@microsoft.com>
2018-04-05 10:27:51 -0700

    a

--- tools/vsts.sh
+++ tools/vsts.sh
@@ -4,9 +4,8 @@ if [ $1 == "Bootstrap" ]; then
     pwsh -command ". ./vsts.ps1; Invoke-PSBootstrap"
 elif [ $1 == "Build" ]; then
     pwsh -command ". ./vsts.ps1; Invoke-PSBuild"
-elif [ $1 == "Test" ]; then {
-    pwsh -command ". ./vsts.ps1; Invoke-PSTest" &&
-} || { return 0 }
+elif [ $1 == "Test" ]; then
+    { pwsh -command ". ./vsts.ps1; Invoke-PSTest" } || { return 0 }
 elif [ $1 == "AfterTest" ]; then
     pwsh -command ". ./vsts.ps1; Invoke-PSAfterTest"
 elif [ $1 == "Add" ]; then
