8c7b8ec06cc8016cc4485f2018d15c198316449f
kalgiz <klalgiz@microsoft.com>
2018-04-16 10:05:44 -0700

    Add Locale settings on vsts machines.

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Install dependencies and clean up
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        locales\
+    && rm -rf /var/lib/apt/lists/*
+
+# Setup the locale
+ENV LANG en_US.UTF-8
+ENV LC_ALL $LANG
+RUN locale-gen $LANG && update-locale
+
 ./install-powershell.sh
 nvm install 6.4.0
 npm install -g markdown-spellcheck@0.11.0;
