89fa3ac71a61add9212b2460dbae7c979a257ffc
kalgiz <klalgiz@microsoft.com>
2018-04-16 16:59:34 -0700

    Remove locale code in install-tools.sh

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # Install dependencies and clean up
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends \
-        locales\
-    && rm -rf /var/lib/apt/lists/*
-
-# Setup the locale
-ENV LANG en_US.UTF-8
-ENV LC_ALL $LANG
-RUN locale-gen $LANG && update-locale
 
 ./install-powershell.sh
 nvm install 6.4.0
